f55083f140622a4e0a231b26a4923a62979e990f
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2023-10-23 20:55:28 +0300

    update logic

--- src/mock-server.ts
+++ src/mock-server.ts
@@ -1,24 +1,49 @@
 import { WebSocketServer } from 'ws';
+import { createReadStream } from 'node:fs';
+import { createInterface } from 'readline';
+import path from 'path';
 
 const wss = new WebSocketServer({ port: 8080 });
+
+async function* generatorFromJsonSample(filePath: string): AsyncGenerator<string> {
+  const sample = require(path.join(__dirname, filePath));
+
+  for (let messageId = 1; messageId < sample._webSocketMessages.length; messageId++) {
+    yield sample._webSocketMessages[messageId].data;
+  }
+}
+
+async function* generatorFromLogSample(filePath: string): AsyncGenerator<string> {
+  const stream = createReadStream(path.join(__dirname, filePath));
+
+  const rl = createInterface({
+    input: stream,
+    crlfDelay: Infinity,
+  });
+
+  for await (const line of rl) {
+    yield line;
+  }
+}
 
 wss.on('connection', function connection(ws, req) {
   console.log('Client connected');
 
-  let sample: any; // This is bad, but it's just a sample
+  let sampleGenerator: AsyncGenerator<string>;
 
   if (req.url === '/endurance') {
-    sample = require('../sample/endurance-timing-modified.json');
+    sampleGenerator = generatorFromJsonSample('../sample/endurance-timing.json');
   } else if (req.url === '/training-1') {
-    sample = require('../sample/pre-endurance-trainings-20-55.json')
+    sampleGenerator = generatorFromJsonSample('../sample/pre-endurance-trainings-20-55.json')
   } else if (req.url === '/training-2') {
-    sample = require('../sample/pre-endurance-trainings-21-21.json')
+    sampleGenerator = generatorFromJsonSample('../sample/pre-endurance-trainings-21-21.json')
+  } else if (req.url === '/race') {
+    sampleGenerator = generatorFromLogSample('../sample/race-22-10-2023.log')
   } else {
     console.error("No sample found for this URL");
     return ws.close();
   }
 
-  let messageId = 1;
   let interval: NodeJS.Timeout;
 
   ws.on('error', (error) => {
@@ -35,13 +60,15 @@ wss.on('connection', function connection(ws, req) {
     console.log('received: %s', data);
 
     if (data.includes("$type\":\"BcStart\",\"ClientKey\":\"2gcircuit\"")) {
-      interval = setInterval(() => {
-        if (messageId < sample._webSocketMessages.length) {
-          return ws.send(sample._webSocketMessages[messageId++].data);
+      interval = setInterval(async () => {
+        const { value, done } = await sampleGenerator.next();
+
+        if (!done) {
+          return ws.send(value);
         }
 
         ws.close();
-      }, 5000);
+      }, 500);
     }
   });
 });
